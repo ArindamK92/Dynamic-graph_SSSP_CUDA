93df6abbfc499bea537698faa62ac6e323b51f51
Arindam <arindamkhanda92@gmail.com>
2020-02-26 01:58:50 -0600

    latest files v1

--- all_structure_cuda.cuh
+++ all_structure_cuda.cuh
@@ -52,12 +52,7 @@ struct xEdge_cuda {
 	double edge_wt;
 	//End of Edge related parameters
 	int inst;
-	bool insertedToDatastructure;
 
-	xEdge_cuda()
-	{
-		insertedToDatastructure = false;
-	}
 	void clear()
 	{}
 };
@@ -103,30 +98,11 @@ struct RT_Vertex
 	int Root;    //root fo the tree
 	int Parent; //mark the parent in the tree
 	double EDGwt; //mark weight of the edge
-	int Level; //Distance from the root
 	int marked; //whether the vertex and the edge connecting its parent ..
 				//..exists(-1); has been deleted(-2); is marked for replacement (+ve value index of changed edge)
-	/*vector<int> PossN;*/ //possible neighbor to check for /**can't use vector in device. need workaround if PossN is necessary
 
 	double Dist;  //Distance from root
 	bool Update;  //Whether the distance of this edge was updated
-	int degree;
-	////Constructor
-	//RT_Vertex()
-	//{
-	//	Root = -1;
-	//	Parent = -1;
-	//	EDGwt = 0.0;
-	//	Level = -1;
-	//	marked = -1;
-
-	//	/*PossN.clear();*/
-	//	Dist = 0.0;
-	//	Update = false;
-	//}
-	////Destructor
-	//void clear()
-	//{}
 };
 //The Rooted tree is a vector of structure RT_Vertex;
 
@@ -135,185 +111,6 @@ struct RT_Vertex
 //Node starts from 0
 //Total number of vertices=nodes and are consecutively arranged
 //reads only the edges in the edge list, does not reverse them to make undirected
-void readin_graphU(A_Network* X, int nodes, char* myfile)
-{
-
-	//Initialize for X
-	/*ADJ_Bundle AList;*/ //create current Adj_Bundle
-	/*X->resize(nodes, AList);*/
-	int_double colvals;
-	int_double colvals2;
-	int node1, node2;
-	double edge_wt;
-
-
-	/////my fn
-	string fileName = myfile;
-	string line;
-	ifstream fin;
-
-	// by default open mode = ios::in mode 
-	fin.open(fileName);
-
-	// Execute a loop until EOF (End of File) 
-	/*vector<float> inputList;*/
-	/*vector<int_double> ListW;*/
-	int current_node = 0;
-	int i = 0;
-	vector<float> inputList;
-	//Read line 
-	while (fin) {
-
-		// Read a Line from File 
-		getline(fin, line);
-		istringstream iss(line);
-		std::string word;
-
-		while (iss >> word)
-		{
-			inputList.push_back(stof(word));
-			if (i % 3 == 0)
-			{
-				node1 = (int)inputList.at(i);
-				colvals2.first = node1;
-				i++;
-
-			}
-			else if (i % 3 == 1)
-			{
-				colvals.first = (int)inputList.at(i);
-				node2 = colvals.first;
-				i++;
-			}
-			else {
-				colvals.second = inputList.at(i);
-				colvals2.second = inputList.at(i);
-				X->at(node1).ListW.push_back(colvals);
-				X->at(node2).ListW.push_back(colvals2);
-				i = 0;
-				inputList.clear();
-			}
-
-		}
-	}
-	/*for (float a : inputList)
-	{
-		cout << a << endl;
-	}*/
-	/*for (int i = 0; i < inputList.size() - 3; i++)
-	{
-		if (i % 3 == 0)
-		{
-			node1 = (int)inputList.at(i);
-			colvals2.first = node1;
-
-		}
-		else if (i % 3 == 1)
-		{
-			colvals.first = (int)inputList.at(i);
-			node2 = colvals.first;
-		}
-		else {
-			colvals.second = inputList.at(i);
-			colvals2.second = inputList.at(i);
-			X->at(node1).ListW.push_back(colvals);
-			X->at(node2).ListW.push_back(colvals2);
-		}
-	}*/
-}
-
-/**** Reading File to Create Network******/
-template <class myNetworkType>
-void readin_network(myNetworkType* X, char* file, int nodes)
-{
-	ADJ_Bundle aBundle;
-	/*X->resize(nodes, aBundle);*/
-	vector<Edge> a;//list of all edges of network
-	Edge myedge;
-	string fileName = file;
-	string line;
-	ifstream fin;
-
-	// by default open mode = ios::in mode 
-	fin.open(fileName);
-	int i = 0;
-	vector<float> inputList;
-	int node1;
-	int_double colvals;
-
-	//Read line 
-	while (fin) {
-
-		// Read a Line from File 
-		getline(fin, line);
-		istringstream iss(line);
-		std::string word;
-
-		while (iss >> word)
-		{
-			inputList.push_back(stof(word));
-			if (i % 3 == 0)
-			{
-				node1 = (int)inputList.at(i);
-				i++;
-
-			}
-			else if (i % 3 == 1)
-			{
-				colvals.first = (int)inputList.at(i);
-				i++;
-			}
-			else {
-				colvals.second = inputList.at(i);
-				X->at(node1).ListW.push_back(colvals);
-				i = 0;
-				inputList.clear();
-
-			}
-		}
-	}
-	/*for (int i = 0; i < inputList.size() - 3; i++)
-	{
-
-		if (i % 3 == 0)
-		{
-			myedge.node1 = (int)inputList.at(i);
-
-		}
-		else if (i % 3 == 1)
-		{
-			myedge.node2 = (int)inputList.at(i);
-
-		}
-		else {
-			myedge.edge_wt = inputList.at(i);
-			a.push_back(myedge);
-
-		}
-	}*/
-
-	/*for (int i = 0; i < a.size(); i++)
-	{
-		cout << "node 1: " << a.at(i).node1 << "node 2: " << a.at(i).node2 << "weight: " << a.at(i).edge_wt << endl;
-	}*/
-	/*create_Network(&a, 0, X);*/
-
-	return;
-}
-
-
-//void create_Network(vector<Edge>* b, int bf_size, A_Network* X)
-//{
-//	for (int i = 0; i < b->size(); i++)
-//	{
-//		int row = b->at(i).node1;
-//		int_double colWt;
-//		colWt.first = b->at(i).node2;
-//		colWt.second = b->at(i).edge_wt;
-//
-//		X->at(row).ListW.push_back(colWt);
-//	}
-//}
 
 
 
@@ -372,31 +169,88 @@ void readin_changes(char* myfile, vector<xEdge>* allChange)
 		}
 	}
 
-	/*for (int i = 0; i < inputList.size() - 4; i++)
-	{
-		if (i % 4 == 0)
-		{
-			myedge.theEdge.node1 = (int)inputList.at(i);
-		}
-		else if (i % 4 == 1)
-		{
-			myedge.theEdge.node2 = (int)inputList.at(i);
-		}
-		else if (i % 4 == 2) {
-			myedge.theEdge.edge_wt = inputList.at(i);
-		}
-		else {
-			ins = (int)inputList.at(i);
-			if (ins == 1)
-			{
-				myedge.inst = true;
-			}
-			else
-			{
-				myedge.inst = false;
-			}
-			allChange->push_back(myedge);
-		}
-	}*/
+
 	return;
 }//end of function
+
+
+void readin_graphU2(A_Network* X, int nodes, char* myfile)
+{
+	FILE* graph_file;
+	char line[128];
+
+	graph_file = fopen(myfile, "r");
+	int l = 0;
+	int prev_node = 0;
+	int_double dummy;
+	dummy.first = 1;
+	dummy.second = 0;
+	vector <int_double> ListW;
+	ADJ_Bundle adjnode;
+
+	while (fgets(line, 128, graph_file) != NULL)
+	{
+		int n1, n2;
+		int wt;
+		//Read line
+		sscanf(line, "%d %d %d", &n1, &n2, &wt);
+		//cout <<n1<<"\n";
+		//Number of nodes given in the first line
+//        if(l==0)
+//        {l++; continue;}
+		if (prev_node != n1)
+		{
+			adjnode.Row = prev_node;
+			adjnode.ListW = ListW;
+			X->push_back(adjnode);
+			/*X->at(prev_node).ListW = ListW;*/
+			ListW.clear();
+			adjnode.clear();
+		}
+		prev_node = n1;
+		dummy.first = n2;
+		dummy.second = (double)wt;
+		ListW.push_back(dummy);
+	}//end of while
+	adjnode.Row = prev_node;
+	adjnode.ListW = ListW;
+	X->push_back(adjnode);
+	fclose(graph_file);
+
+	return;
+}
+
+void readin_graphU4(int* colStartPtr, Colwt2* cuda_adjlist_full, char* myfile, int* nodes)
+{
+	FILE* graph_file;
+	char line[128];
+
+	graph_file = fopen(myfile, "r");
+	int l = 0;
+	int prev_node = 0;
+	int_double dummy;
+	dummy.first = 1;
+	dummy.second = 0;
+	vector <int_double> ListW;
+	ADJ_Bundle adjnode;
+	colStartPtr[0] = 0;
+	while (fgets(line, 128, graph_file) != NULL)
+	{
+		int n1, n2, wt;
+		//Read line
+		sscanf(line, "%d %d %d", &n1, &n2, &wt);
+		if (prev_node != n1)
+		{
+			colStartPtr[n1] = l;
+		}
+		prev_node = n1;
+		cuda_adjlist_full[l].col = n2;
+		cuda_adjlist_full[l].wt = (double)wt;
+		l++;
+
+	}//end of while
+	colStartPtr[*nodes] = l; //last element stores dummy last value to indicate the end of the ptr
+	fclose(graph_file);
+
+	return;
+}
